d28275c9acbc38de2182bd90214d64fd8aba2bce
SamanvithKashyap <61279872+SamanvithKashyap@users.noreply.github.com>
2020-03-13 21:47:43 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -80,7 +80,7 @@ function draw() {
     ground.velocityX = -(6 + 3*score/100);
   
     if(keyDown("space") && trex.y >= 159) {
-      trex.velocityY = -12;
+      trex.velocityY = -15;
     }
     
   
